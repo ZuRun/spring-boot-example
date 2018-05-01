ee12bba0252bcdee9c214438da5950d0f03c3f40
ZuRun's MacBook Pro <hi@zuhr.me>
2018-05-01 23:13:41 +0800

    mybatis-plus

---
+++ common/mybatis-plus/src/main/java/me/zuhr/demo/mybatis/mapper/Mapper.java
@@ -0,0 +1,11 @@
+package me.zuhr.demo.mybatis.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * @author zurun
+ * @date 2018/5/1 22:59:46
+ */
+public interface Mapper<T> extends BaseMapper<T> {
+
+}
