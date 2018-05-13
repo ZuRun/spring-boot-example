c482e1597aaff4a78a86d0ad3b2cd0676be54149
ZuRun's MacBook Pro <hi@zuhr.me>
2018-05-14 01:04:27 +0800

    模块变更,解决循环依赖等问题

--- model-test/eureka-client-provider/eureka-client-provider-server/src/main/java/me/zuhr/demo/ecp/controller/ExceptionController.java
+++ model-test/eureka-client-provider/eureka-client-provider-server/src/main/java/me/zuhr/demo/ecp/controller/ExceptionController.java
@@ -6,8 +6,6 @@ import me.zuhr.demo.ecp.api.ExceptionRemoteApi;
 import me.zuhr.demo.ecp.constants.ErrorCode;
 import me.zuhr.demo.server.restful.MyRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
